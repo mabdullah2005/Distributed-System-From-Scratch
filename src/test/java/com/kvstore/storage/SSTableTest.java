bba21522d1f2ce4ce95a4ed761621f4ee6158bf0
mabdullah2005 <mabdullah2005967@gmail.com>
2026-09-08 16:50:52 +0100

    log-05: Add SSTable Unit Testing

---
+++ src/test/java/com/kvstore/storage/SSTableTest.java
@@ -0,0 +1,44 @@
+package com.kvstore.storage;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.util.List;
+
+public class SSTableTest{
+    private MemTable memTable;
+    private SSTable ssTable;
+
+    @TempDir
+    private Path tempDir;
+    private String pathFile;
+
+    @BeforeEach
+    void setUp(){
+        pathFile = tempDir.resolve("test_ssTable.log").toString();
+        memTable = new MemTable();
+        ssTable = new SSTable();
+    }
+
+    @Test
+    void ssTable_flush() throws IOException {
+        memTable.put("Rhino", "grey");
+        memTable.put("Zebra", "black and white");
+        memTable.put("Ant", "brown");
+
+        ssTable.flushToDisk(memTable, pathFile);
+
+        List<String> lines = java.nio.file.Files.readAllLines(Paths.get(pathFile));
+
+        assertEquals("Ant|brown", lines.get(0));
+        assertEquals("Rhino|grey", lines.get(1));
+        assertEquals("Zebra|black and white", lines.get(2));
+    }
+}
